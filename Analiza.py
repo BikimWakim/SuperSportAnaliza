e39d02ab8dc03e230fe3a12c6d08d3257abdd655
BikimWakim <bornas2000@gmail.com>
2024-03-18 21:15:10 +0100

    Initial commit

---
+++ Analiza.py
@@ -0,0 +1,12 @@
+import csv
+import pandas as pd
+
+
+
+
+
+#column_names = ['Index', 'Id', 'Coefficient', 'Match', 'Outcome', 'Date', 'Bet Type', ' Sport', 'User']
+
+df = pd.read_csv('C:/Users/borna/Documents/analize2.csv')
+
+print(df.head())
